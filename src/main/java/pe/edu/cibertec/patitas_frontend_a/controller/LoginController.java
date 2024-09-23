50e7f1cc608f56d495177f673267c8e2f23bc52d
Daniel Isidro <isidroesteban402@gmail.com>
2024-09-23 13:13:04 -0500

    Consumiendo API del BACK-END

--- src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/LoginController.java
@@ -1,17 +1,22 @@
 package pe.edu.cibertec.patitas_frontend_a.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 import pe.edu.cibertec.patitas_frontend_a.viewmodel.LoginModel;
+import pe.edu.cibertec.patitas_frontend_a.viewmodel.LoginRequest;
 
 @Controller
 @RequestMapping("/login")
 public class LoginController {
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/inicio")
     public String inicio(Model model) {
@@ -28,17 +33,25 @@ public class LoginController {
                              Model model) {
 
         //validar campos de entrada
-        if(tipoDocumento == null || tipoDocumento.trim().length() ==0 ||
-           numeroDocumento == null || numeroDocumento.trim().length() ==0 ||
-           password == null || password.trim().isEmpty()){
-            LoginModel loginModel = new LoginModel("01", "RELLENAAAAAAAAAAAAAAA", "Daniel Isidro");
+        if (tipoDocumento == null || tipoDocumento.trim().length() == 0 ||
+                numeroDocumento == null || numeroDocumento.trim().length() == 0 ||
+                password == null || password.trim().isEmpty()) {
+            LoginModel loginModel = new LoginModel("01", "Por favor, complete todos los campos.", "");
             model.addAttribute("loginModel", loginModel);
             return "inicio";
         }
-        LoginModel loginModel = new LoginModel("00", "", "Daniel Isidro");
-        model.addAttribute("loginModel", loginModel);
-        return "principal";
 
+        String url = "http://localhost:8081/autentificacion/login"; //url del backend
+        LoginRequest LoginReqDTO = new LoginRequest(tipoDocumento, numeroDocumento, password);
+        LoginModel loginModel = restTemplate.postForObject(url, LoginReqDTO , LoginModel.class);
+
+        // Verificar la respuesta del backend
+        if (loginModel != null && "00".equals(loginModel.codigo())) {
+            model.addAttribute("loginModel", loginModel);
+            return "principal";
+        } else {
+            model.addAttribute("loginModel", new LoginModel("01", "Credenciales incorrectas.", ""));
+            return "inicio";
+        }
     }
-
 }
---
+++ src/main/java/pe/edu/cibertec/patitas_frontend_a/controller/RestTemplateConfig.java
@@ -0,0 +1,14 @@
+package pe.edu.cibertec.patitas_frontend_a.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
